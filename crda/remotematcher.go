2a214059c9518aa77cddca4c6556e5132d3b264c
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-07-17 20:50:57 +0530

    crda-remotematcher: Use CRDA V2 API to access Snyk data

--- crda/remotematcher.go
+++ crda/remotematcher.go
@@ -1,108 +1,144 @@
 package crda
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/rs/zerolog"
 
 	"github.com/quay/claircore"
 )
 
-const (
-	batchSize = 10
-	url       = `https://f8a-analytics-2445582058137.production.gw.apicast.io:443/api/v1/component-analyses/?user_key=9e7da76708fe374d8c10fa752e72989f`
+var (
+	urlTemplate = url.URL{
+		Scheme:   "https",
+		Host:     "f8a-analytics-2445582058137.production.gw.apicast.io",
+		Path:     "/api/v2/component-analyses/pypi/%s/%s",
+		RawQuery: "user_key=9e7da76708fe374d8c10fa752e72989f",
+	}
 )
 
-// Build struct to model CRDA ComponentAnalysis response.
-
+// Build struct to model CRDA V2 ComponentAnalysis response which
+// delivers Snyk sourced Vulnerability information.
 type Vulnerability struct {
-	ID			string	`json:"vendor_cve_ids"`
-	CVSS		string	`json:"cvss"`
+	ID   string `json:"vendor_cve_ids"`
+	CVSS string `json:"cvss"`
 }
 
 type Analyses struct {
-	Vulnerabilities	[]Vulnerability	`json:"vulnerability"`
+	Vulnerabilities []Vulnerability `json:"vulnerability"`
 }
 
-type Report struct {
-	RecommendationVersion string		`json:"recommended_versions"`
-	Severity							string		`json:"severity"`
-	Message							  string		`json:"message"`
-	Analyses							Analyses	`json:"component_analyses"`
+type VulnReport struct {
+	RecommendedVersion string   `json:"recommended_versions"`
+	Severity           string   `json:"severity"`
+	Message            string   `json:"message"`
+	Analyses           Analyses `json:"component_analyses"`
+	// To map back to input
+	record *claircore.IndexRecord
 }
 
-func call(records *claircore.IndexRecord, c chan Report) {
-	fmt.Println("Inside call")
-	var req []Request
-	for _, record := range records {
-		req = append(req, Request{Ecosystem: "pypi",
-			Package: record.Package.Name,
-			Version: record.Package.Version})
+func componentAnalyses(ctx context.Context, record *claircore.IndexRecord, c chan VulnReport) {
+	log := zerolog.Ctx(ctx).With().
+		Str("component", "crda/remotematcher/componentAnalyses").
+		Logger()
+	reqUrl := urlTemplate
+	reqUrl.Path = fmt.Sprintf(reqUrl.Path, record.Package.Name, record.Package.Version)
+	req := http.Request{
+		Method:     http.MethodGet,
+		Header:     http.Header{"User-Agent": {"claircore/crda/RemoteMatcher"}},
+		URL:        &reqUrl,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Host:       reqUrl.Host,
 	}
-	jsonValue, _ := json.Marshal(req)
-	response, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+	// Per request shouldn't go beyound 10s.
+	tctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	log.Debug().
+		Str("package", record.Package.Name).
+		Str("version", record.Package.Version).
+		Msg("restful")
+		// Fixme: Use configurable http client.
+	res, err := http.DefaultClient.Do(req.WithContext(tctx))
+	if res != nil {
+		defer res.Body.Close()
+	}
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		log.Error().
+			Err(err).
+			Str("package", record.Package.Name).
+			Str("version", record.Package.Version).
+			Msg("restful")
+		c <- VulnReport{record: record}
 	} else {
-		var da_response []Report
-		data, _ := ioutil.ReadAll(response.Body)
-		err = json.Unmarshal(data, &da_response)
+		var vulnReport VulnReport
+		data, _ := ioutil.ReadAll(res.Body)
+		err = json.Unmarshal(data, &vulnReport)
 		if err != nil {
-			fmt.Println(err)
+			log.Error().
+				Err(err).
+				Str("package", record.Package.Name).
+				Str("version", record.Package.Version).
+				Msg("unmarshal")
+			c <- VulnReport{record: record}
 		}
-		result := ReportsID{Response: da_response, Request: records}
-		c <- result
+		vulnReport.record = record
+		c <- vulnReport
 	}
 }
 
 func QueryRemoteMatcher(ctx context.Context, records []*claircore.IndexRecord) (map[string][]*claircore.Vulnerability, error) {
 	log := zerolog.Ctx(ctx).With().
-		Str("component", "internal/vulnstore/dastore/get").
+		Str("component", "crda/remotematcher/QueryRemoteMatcher").
 		Logger()
 	ctx = log.WithContext(ctx)
-	ch := make(chan ReportsID)
-	recordLen := len(records)
-	// CRDA remote matcher post API can process `batchSize` records at a time.
-	for i := 0; i < recordLen; i += batchSize {
-		j := i + batchSize
-		if j > recordLen {
-			j = recordLen
-		}
-		go call(records[i:j], ch)
+	log.Debug().
+		Int("records", len(records)).
+		Msg("packages")
+
+	ch := make(chan VulnReport)
+	for _, record := range records {
+		go componentAnalyses(ctx, record, ch)
 	}
 
 	results := make(map[string][]*claircore.Vulnerability)
-	batchLen := int(math.Ceil(float64(recordLen) / float64(batchSize)))
-	for i := 0; i < batchLen; i++ {
-		res, ok := <-ch
-		if !ok {
-			break
+	resultCount := 0
+	for r := range ch {
+		resultCount++
+		// A package can have multiple vulnerability for a version.
+		for _, vuln := range r.Analyses.Vulnerabilities {
+			results[r.record.Package.ID] = append(results[r.record.Package.ID], &claircore.Vulnerability{
+				ID:          vuln.ID,
+				Updater:     "Code Ready Analytics",
+				Name:        vuln.ID,
+				Description: fmt.Sprintf("%s cvss: %s", r.Message, vuln.CVSS),
+				// Fixme: Links should be part of response.
+				Links:              fmt.Sprintf("https://snyk.io/vuln/%s", vuln.ID),
+				Severity:           r.Severity,
+				NormalizedSeverity: NormalizeSeverity(r.Severity),
+				FixedInVersion:     r.RecommendedVersion,
+				Package:            r.record.Package,
+			})
+			log.Debug().
+				Str("package", r.record.Package.Name).
+				Str("version", r.record.Package.Version).
+				Str("id", vuln.ID).
+				Msg("vulns")
 		}
-
-		req := res.Request
-		response := res.Response
-		for i, r := range response {
-			var vulnArray []*claircore.Vulnerability
-			// A package can have multiple vulnerability for single version.
-			for _, cve := range r.Result.Recommendation.ComponentAnalysis.Cve {
-				vulnArray = append(vulnArray, &claircore.Vulnerability{
-					ID:             cve.ID,
-					Updater:        "crda",
-					Name:           cve.ID,
-					Description:    r.Result.Recommendation.Message,
-					Severity:       fmt.Sprint(cve.Cvss),
-					FixedInVersion: r.Result.Recommendation.ChangeTo,
-					Package:        req[i].Package,
-				})
-			}
-			results[req[i].Package.ID] = vulnArray
+		// Got result for all records.
+		if resultCount >= len(records) {
+			close(ch)
 		}
 	}
+	log.Debug().
+		Int("vulnerabilities", len(results)).
+		Msg("query")
 	return results, nil
 }
