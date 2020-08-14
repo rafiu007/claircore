786bf996ff8391ed8d9de0dbb8fbf493e6d88390
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-08-15 01:43:03 +0530

    fix: Use bounded concurrency and simplify pipeline

--- crda/remotematcher.go
+++ crda/remotematcher.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -17,12 +18,17 @@ import (
 
 var (
 	urlTemplate = url.URL{
-		Scheme:   "https",
-		Host:     "f8a-analytics-preview-2445582058137.production.gw.apicast.io",
-		Path:     "/api/v2/component-analyses/pypi/%s/%s",
+		Scheme: "https",
+		// TODO: Host must be configurable
+		Host: "f8a-analytics-preview-2445582058137.production.gw.apicast.io",
+		Path: "/api/v2/component-analyses/pypi/%s/%s",
+		// TODO: Userkey must be configurable
 		RawQuery: "user_key=3e42fa66f65124e6b1266a23431e3d08",
 	}
 )
+
+// Bounded concurrency limit.
+const concurrencyLimit = 10
 
 // Build struct to model CRDA V2 ComponentAnalysis response which
 // delivers Snyk sourced Vulnerability information.
@@ -44,14 +50,9 @@ type VulnReport struct {
 	RecommendedVersion string   `json:"recommended_versions"`
 	Message            string   `json:"message"`
 	Analyses           Analyses `json:"component_analyses"`
-	// To map back to input
-	record *claircore.IndexRecord
 }
 
-func componentAnalyses(ctx context.Context, record *claircore.IndexRecord, c chan VulnReport) {
-	log := zerolog.Ctx(ctx).With().
-		Str("component", "crda/remotematcher/componentAnalyses").
-		Logger()
+func componentAnalyses(ctx context.Context, record *claircore.IndexRecord) ([]claircore.Vulnerability, error) {
 	reqUrl := urlTemplate
 	reqUrl.Path = fmt.Sprintf(reqUrl.Path, record.Package.Name, record.Package.Version)
 	req := http.Request{
@@ -66,36 +67,36 @@ func componentAnalyses(ctx context.Context, record *claircore.IndexRecord, c cha
 	// Per request shouldn't go beyound 10s.
 	tctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
-	log.Debug().
-		Str("package", record.Package.Name).
-		Str("version", record.Package.Version).
-		Msg("restful")
-		// Fixme: Use configurable http client.
+	// Fixme: Use configurable http client.
 	res, err := http.DefaultClient.Do(req.WithContext(tctx))
 	if res != nil {
 		defer res.Body.Close()
 	}
 	if err != nil {
-		log.Error().
-			Err(err).
-			Str("package", record.Package.Name).
-			Str("version", record.Package.Version).
-			Msg("restful")
-		c <- VulnReport{record: record}
+		return nil, err
 	} else {
 		var vulnReport VulnReport
 		data, _ := ioutil.ReadAll(res.Body)
 		err = json.Unmarshal(data, &vulnReport)
 		if err != nil {
-			log.Error().
-				Err(err).
-				Str("package", record.Package.Name).
-				Str("version", record.Package.Version).
-				Msg("unmarshal")
-			c <- VulnReport{record: record}
+			return nil, err
 		}
-		vulnReport.record = record
-		c <- vulnReport
+		// A package can have multiple vulnerability for a version.
+		var vulns []claircore.Vulnerability
+		for _, vuln := range vulnReport.Analyses.Vulnerabilities {
+			vulns = append(vulns, claircore.Vulnerability{
+				ID:                 vuln.ID,
+				Updater:            "CodeReadyAnalytics",
+				Name:               vuln.ID,
+				Description:        fmt.Sprintf("%s(cvss: %s)(cves: %s)\n%s", vuln.Title, vuln.CVSS, strings.Join(vuln.CVES, ","), vulnReport.Message),
+				Links:              vuln.URL,
+				Severity:           vuln.Severity,
+				NormalizedSeverity: NormalizeSeverity(vuln.Severity),
+				FixedInVersion:     strings.Join(vuln.FixedIn, ","),
+				Package:            record.Package,
+			})
+		}
+		return vulns, nil
 	}
 }
 
@@ -108,37 +109,38 @@ func QueryRemoteMatcher(ctx context.Context, records []*claircore.IndexRecord) (
 		Int("records", len(records)).
 		Msg("packages")
 
-	ch := make(chan VulnReport)
-	for _, record := range records {
-		go componentAnalyses(ctx, record, ch)
-	}
+	inC := make(chan *claircore.IndexRecord, concurrencyLimit)
+	ctrlC := make(chan []claircore.Vulnerability, concurrencyLimit)
+	errorC := make(chan error)
+	go func() {
+		defer close(ctrlC)
+		var g errgroup.Group
+		for _, record := range records {
+			g.Go(func() error {
+				vulns, err := componentAnalyses(ctx, <-inC)
+				if err != nil {
+					return err
+				}
+				ctrlC <- vulns
+				return nil
+			})
+			inC <- record
+		}
+		close(inC)
+		if err := g.Wait(); err != nil {
+			errorC <- err
+		}
+	}()
 
 	results := make(map[string][]*claircore.Vulnerability)
-	resultCount := 0
-	for r := range ch {
-		resultCount++
-		// A package can have multiple vulnerability for a version.
-		for _, vuln := range r.Analyses.Vulnerabilities {
-			results[r.record.Package.ID] = append(results[r.record.Package.ID], &claircore.Vulnerability{
-				ID:                 vuln.ID,
-				Updater:            "CodeReadyAnalytics",
-				Name:               vuln.ID,
-				Description:        fmt.Sprintf("%s(cvss: %s)(cves: %s)\n%s", vuln.Title, vuln.CVSS, strings.Join(vuln.CVES, ","), r.Message),
-				Links:              vuln.URL,
-				Severity:           vuln.Severity,
-				NormalizedSeverity: NormalizeSeverity(vuln.Severity),
-				FixedInVersion:     strings.Join(vuln.FixedIn, ","),
-				Package:            r.record.Package,
-			})
+	for r := range ctrlC {
+		for _, vuln := range r {
+			results[vuln.Package.ID] = append(results[vuln.Package.ID], &vuln)
 			log.Debug().
-				Str("package", r.record.Package.Name).
-				Str("version", r.record.Package.Version).
+				Str("package", vuln.Package.Name).
+				Str("version", vuln.Package.Version).
 				Str("id", vuln.ID).
 				Msg("vulns")
-		}
-		// Got result for all records.
-		if resultCount >= len(records) {
-			close(ch)
 		}
 	}
 	log.Debug().
